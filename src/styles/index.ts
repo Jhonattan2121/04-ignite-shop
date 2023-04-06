4edee3b71ce7b1a24a9ad3dbd6c4264c22798af2
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-04-06 15:51:41 -0300

    Estilos globais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,7 +4,15 @@ export const {
     config, styled, css ,globalCss, keyframes, getCssText , theme , createTheme } = createStitches({
     theme: {
         colors: {
-           rocketseat: '#8257e6',
+            white: '#fff',
+            
+            gray100: '#e1e1e6',
+            gray300: '#c4c4cc',
+            gray800: '#202024',
+            gray900: '#121214',
+
+            green500: '#00875f',
+            green300: '#00b37e',
         }
     }
 })
