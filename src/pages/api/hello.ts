546a389ce5c3aaed50901f2d8ccf763cb53f600d
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-04-27 00:09:25 -0300

    API routes no Next.js

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  return response.json({
+    message: 'Hello World!',
+  })
+}
